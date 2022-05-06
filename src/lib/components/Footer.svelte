b9f9524eb182c1102d68a365138e9f1daae1bc89
Jack Buscombe <jackbuscombe94@gmail.com>
2022-05-07 05:20:36 +1000

    Moved Section Background Image Url from Tailwind Config to Prop Declaration

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <footer
-	class="flex flex-1 justify-center space-x-6 text-gray-600 mb-8 text-lg font-semibold bg-transparent"
+	class="flex flex-1 justify-center space-x-6 text-gray-600 -mt-2 mb-2 text-lg font-semibold bg-transparent"
 >
 	<a href="/">Tesla © 2022</a>
 	<a href="/">Privacy & Legal</a>
